3e9d6e172cf97d4d3642025db3f7a922a3bbde02
Glitch (abounding-thunder) <none>
2018-09-20 18:28:21 +0000

    🌠🏤 Checkpoint ./README.md:284076/802 ./index.js:284076/1996 ./package.json:284076/66

--- README.md
+++ README.md
@@ -14,15 +14,11 @@ changes
 ### Run locally or [![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/remix/slack-greet-and-react-example)
 
 1. Get the code
-	- Either clone this repo and run `npm install`
-	- Or visit [glitch.com/edit/#!/remix/slack-greet-and-react-example](https://glitch.com/edit/#!/remix/slack-greet-and-react-example)
-2. Set the following environment variables (all available on the *Basic Information* page) to `.env` (see `.env.sample` if you're developing locally)
-	- `SLACK_CLIENT_ID`: You app's _Client ID_
-	- `SLACK_CLIENT_SECRET`: Your app's _Client Secret_
-	- `SLACK_VERIFICATION_TOKEN`: Your app's _Verification Token_
-3. If you're running the app locally:
-	- Start the app (`node index.js`)
-	- In another window, start ngrok on the same port as your webserver (`ngrok http $PORT`)
+  - "Remix" the code to your Glitch repo:
+	[glitch.com/edit/#!/remix/slack-greet-and-react-example](https://glitch.com/edit/#!/remix/slack-greet-and-react-example)
+2. Set the environment variables (all available on the *Basic Information* page) to `.env` 
+	- `SLACK_SIGNING_SECRET`: Your app's _Signing Secret_
+
 
 ### Enable Events
 1. Go back to the app settings and click on `Event Subscriptions` on the left side navigation
@@ -30,12 +26,10 @@ changes
 	- ngrok or Glitch URL + `/slack/events`
 3. After you set up the _Request URL_, you should add event subscriptions under the "Bot Events" category. Add `message.channels` and `reaction_added`.
 4. Go to `OAuth & Permissions` item on the left side navigation, and input the _Redirect URL_:
-	- ngrok or Glitch URL + `/auth/slack/callback`
+	- Glitch URL (sould look something like `https://my-stuff.glitch.me/`) + `/auth/slack/callback`
 
 ## Installation and Usage
-1.  Visit the URL of your development proxy
-	- ngrok or Glitch URL
-2. Click the "Add to Slack" button, and complete the installation
+1. Install your integartion to your workspace
 3. You can invite the Bot User into a channel (e.g. `/invite @greetandreact`), and say "hi" in the
 channel. It should respond to you. You can also add reactions to messages and the Bot User will send
 a message using the same emoji.
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 
 const slackEventsApi = require('@slack/events-api');
 const SlackClient = require('@slack/client').WebClient;
-const passport = require('passport');
-const SlackStrategy = require('@aoberoi/passport-slack').default.Strategy;
 const http = require('http');
 const express = require('express');
 
@@ -13,54 +11,12 @@ const slackEvents = slackEventsApi.createEventAdapter(process.env.SLACK_SIGNING_
   includeBody: true
 });
 
-// Initialize a data structures to store team authorization info (typically stored in a database)
-const botAuthorizations = {}
 
-// Helpers to cache and lookup appropriate client
-// NOTE: Not enterprise-ready. if the event was triggered inside a shared channel, this lookup
-// could fail but there might be a suitable client from one of the other teams that is within that
-// shared channel.
-const clients = {};
-function getClientByTeamId(teamId) {
-  if (!clients[teamId] && botAuthorizations[teamId]) {
-    clients[teamId] = new SlackClient(botAuthorizations[teamId]);
-  }
-  if (clients[teamId]) {
-    return clients[teamId];
-  }
-  return null;
-}
-
-// Initialize Add to Slack (OAuth) helpers
-passport.use(new SlackStrategy({
-  clientID: process.env.SLACK_CLIENT_ID,
-  clientSecret: process.env.SLACK_CLIENT_SECRET,
-  skipUserProfile: true,
-}, (accessToken, scopes, team, extra, profiles, done) => {
-  botAuthorizations[team.id] = extra.bot.accessToken;
-  done(null, {});
-}));
 
 // Initialize an Express application
 const app = express();
 
-// Plug the Add to Slack (OAuth) helpers into the express app
-app.use(passport.initialize());
-app.get('/', (req, res) => {
-  res.send('<a href="/auth/slack"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
-});
-app.get('/auth/slack', passport.authenticate('slack', {
-  scope: ['bot']
-}));
-app.get('/auth/slack/callback',
-  passport.authenticate('slack', { session: false }),
-  (req, res) => {
-    res.send('<p>Greet and React was successfully installed on your team.</p>');
-  },
-  (err, req, res, next) => {
-    res.status(500).send(`<p>Greet and React failed to install</p> <pre>${err}</pre>`);
-  }
-);
+
 
 // *** Plug the event adapter into the express app as middleware ***
 app.use('/slack/events', slackEvents.expressMiddleware());
