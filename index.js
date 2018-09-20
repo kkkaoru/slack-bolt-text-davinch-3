1b9f9e8993b0a973f4c47bd9c104ad9a45e87173
Glitch (abounding-thunder) <none>
2018-09-20 18:49:44 +0000

    🍅💓 Checkpoint ./index.js:284076/963

--- index.js
+++ index.js
@@ -6,34 +6,16 @@ const SlackClient = require('@slack/client').WebClient;
 const http = require('http');
 const express = require('express');
 
-// *** Initialize a client with your access token
-const token = process.env.SLACK_ACCESS_TOKEN;
-const slack = new SlackClient(token);
+// *** Initialize an Express application
+const app = express();
 
+// *** Initialize a client with your access token
+const slack = new SlackClient(process.env.SLACK_ACCESS_TOKEN);
 
 // *** Initialize event adapter using signing secret from environment variables ***
 const slackEvents = slackEventsApi.createEventAdapter(process.env.SLACK_SIGNING_SECRET, {
   includeBody: true
 });
-
-
-// Helpers to cache and lookup appropriate client
-// NOTE: Not enterprise-ready. if the event was triggered inside a shared channel, this lookup
-// could fail but there might be a suitable client from one of the other teams that is within that
-// shared channel.
-// const clients = {};
-// function getClientByTeamId(teamId) {
-//   if (!clients[teamId] && botAuthorizations[teamId]) {
-//     clients[teamId] = new SlackClient(botAuthorizations[teamId]);
-//   }
-//   if (clients[teamId]) {
-//     return clients[teamId];
-//   }
-//   return null;
-// }
-
-// Initialize an Express application
-const app = express();
 
 
 // *** Plug the event adapter into the express app as middleware ***
@@ -55,9 +37,11 @@ slackEvents.on('message', (message, body) => {
 
 // *** Responding to reactions with the same emoji ***
 slackEvents.on('reaction_added', (event, body) => {
-
   // Respond to the reaction back with the same emoji
   slack.chat.postMessage(event.item.channel, `:${event.reaction}:`)
+    .then((res) => {
+      console.log('Message sent: ', res.ts);
+    })
     .catch(console.error);
 });
 
