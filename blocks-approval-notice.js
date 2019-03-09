948f3bbacc43b42833de4e40acd2aff56c21be41
Glitch (greet-bot) <none>
2019-03-09 00:50:43 +0000

    🐑🌹 Checkpoint ./blocks-approval-notice.js:3224629/5031

--- blocks-approval-notice.js
+++ blocks-approval-notice.js
@@ -57,161 +57,106 @@ const request = [
 ]
 
 const confirmation = [
-  {
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
-    }
-  },
-  {
-    "type": "section",
-    "fields": [
-      {
-        "type": "mrkdwn",
-        "text": "*Type:*\nComputer (laptop)"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*When:*\nSubmitted Aut 10"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Reason:*\nAll vowel keys aren't working."
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
-      }
-    ]
-  },
-  {
-    "type": "context",
-    "elements": [
-      {
-        "type": "plain_text",
-        "text": ":white_check_mark: Approved by Michael Scott on March 14, 2019 03:14PM",
-        "emoji": true
-      }
-    ]
-  }
+	{
+		"type": "section",
+		"text": {
+			"type": "mrkdwn",
+			"text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
+		}
+	},
+	{
+		"type": "section",
+		"fields": [
+			{
+				"type": "mrkdwn",
+				"text": "*Type:*\nComputer (laptop)"
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*When:*\nSubmitted Aut 10"
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*Reason:*\nAll vowel keys aren't working."
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
+			}
+		]
+	},
+	{
+		"type": "context",
+		"elements": [
+			{
+				"type": "plain_text",
+				"text": ":white_check_mark: Approved by you on 03/14/2015 03:45pm",
+				"emoji": true
+			}
+		]
+	}
 ]
 
-const denialInfo = [
-  {
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
-    }
-  },
-  {
-    "type": "section",
-    "fields": [
-      {
-        "type": "mrkdwn",
-        "text": "*Type:*\nComputer (laptop)"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*When:*\nSubmitted Aut 10"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Reason:*\nAll vowel keys aren't working."
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
-      }
-    ]
-  },
-  {
-    "type": "context",
-    "elements": [
-      {
-        "type": "plain_text",
-        "text": ":x: Declined by Michael Scott March 14, 2019 03:14PM",
-        "emoji": true
-      }
-    ]
-  },
-    {
-    "type": "context",
-    "elements": [
-      {
-        "type": "plain_text",
-        "text": "Reason: Out of Policy request",
-        "emoji": true
-      }
-    ]
-  }
+const denial = [
+	{
+		"type": "section",
+		"text": {
+			"type": "mrkdwn",
+			"text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
+		}
+	},
+	{
+		"type": "section",
+		"fields": [
+			{
+				"type": "mrkdwn",
+				"text": "*Type:*\nComputer (laptop)"
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*When:*\nSubmitted Aut 10"
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*Reason:*\nAll vowel keys aren't working."
+			},
+			{
+				"type": "mrkdwn",
+				"text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
+			}
+		]
+	},
+	{
+		"type": "context",
+		"elements": [
+			{
+				"type": "plain_text",
+				"text": ":x: You declined this request on 03/14/2019 at 03:45pm",
+				"emoji": true
+			}
+		]
+	},
+	{
+		"type": "context",
+		"elements": [
+			{
+				"type": "plain_text",
+				"text": ":information_source: Reason: Out of Policy",
+				"emoji": true
+			}
+		]
+	}
 ]
 
-const denialDone = [
-  {
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
-    }
-  },
-  {
-    "type": "section",
-    "fields": [
-      {
-        "type": "mrkdwn",
-        "text": "*Type:*\nComputer (laptop)"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*When:*\nSubmitted Aut 10"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Reason:*\nAll vowel keys aren't working."
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
-      }
-    ]
-  },
-  {
-    "type": "context",
-    "elements": [
-      {
-        "type": "plain_text",
-        "text": ":x: Declined by Michael Scott March 14, 2019 03:14PM",
-        "emoji": true
-      }
-    ]
-  },
-    {
-    "type": "context",
-    "elements": [
-      {
-        "type": "plain_text",
-        "text": "Reason: Out of Policy request",
-        "emoji": true
-      }
-    ]
-  }
-]
-
-module.exports = { request, confirmation, denialInfo, denialDone }
+module.exports = { request, confirmation, denial }
   
 
 
