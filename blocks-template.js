e8a6b5bd9fa4bc6015b5b448d87bdaea66b00502
Glitch (greet-bot) <none>
2019-03-12 22:04:30 +0000

    🚠🍑 Checkpoint ./blocks-template.js:3224629/10764

---
+++ blocks-template.js
@@ -0,0 +1,113 @@
+module.exports = {
+  message: {
+    start: {
+      channel: "DGGD1E5RA",
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "Click one of these buttons to start a flow :point_down:"
+          }
+        },
+        {
+          "type": "actions",
+          "elements": [
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Trigger message"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"message\",\"value\":\"demo\"}"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Trigger message reply"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"thread\",\"value\":\"demo\"}"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Trigger ephemeral message"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"ephemeral\",\"value\":\"demo\"}"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Update message"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"update\",\"value\":\"demo\"}"
+            }
+          ]
+        }
+      ]
+    }
+  },
+  update: {
+    demo: {
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "You updated this message "
+          }
+        },
+        {
+          "type": "actions",
+          "elements": [
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Trigger message"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"message\",\"value\":\"demo\"}"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Trigger message reply"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"thread\",\"value\":\"demo\"}"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Trigger ephemeral message"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"ephemeral\",\"value\":\"demo\"}"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Update message"
+              },
+              "value": "{\"blueprint\":\"template\",\"type\":\"update\",\"value\":\"demo\"}"
+            }
+          ]
+        }
+      ]
+    }
+  },
+  dialog: {
+  }
+}
