69cb2e38f8243eb33eb7fd4d4df853115c43ffd4
Glitch (greet-bot) <none>
2019-03-13 21:37:49 +0000

    💨😤 Checkpoint ./index.js:3224629/108

--- index.js
+++ index.js
@@ -71,7 +71,9 @@ const handleAction = (payload, value) => {
   // console.log('options', options)
   // add options from current action to the next block's value
   // e.g. for updating the current message after a dialog submission
-  // block = helpers.fillOptions(action, block)
+  // block = helpers.fillOptions(block, payload)
+  console.log(payload)
+  console.log(action)
   
   switch(action.type) {
     case 'dialog':  
