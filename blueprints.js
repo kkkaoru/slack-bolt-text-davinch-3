cb0526a3f22c6a2e309fab54730b40431e54d5fa
Glitch (greet-bot) <none>
2019-03-22 23:37:50 +0000

    🛳🚞 Checkpoint ./blueprints.js:3224629/337

--- blueprints.js
+++ blueprints.js
@@ -5,11 +5,6 @@ const publicPoll = require('./blueprint-public-poll')
 const search = require('./blueprint-search')
 const template = require('./blueprint-template')
 
-const onboarding1 = require('./blueprint-onboarding-1')
-const onboarding2 = require('./blueprint-onboarding-2')
-const onboarding3 = require('./blueprint-onboarding-3')
-const onboarding4 = require('./blueprint-onboarding-4')
-
 const slashCommands = require('./slash-commands')
 
 module.exports = {
@@ -19,9 +14,5 @@ module.exports = {
   publicPoll: publicPoll,
   search: search,
   template: template,
-  slashCommands: slashCommands,
-  onboarding1: onboarding1,
-  onboarding2: onboarding2,
-  onboarding3: onboarding3,
-  onboarding4: onboarding4
+  slashCommands: slashCommands
 }
