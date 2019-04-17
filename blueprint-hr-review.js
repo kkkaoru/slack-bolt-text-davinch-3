7f60cfbdea27ab9951d361d8a4b81af9a87938c5
Glitch (greet-bot) <none>
2019-04-17 18:15:23 +0000

    🕷🌒 Checkpoint ./blueprint-hr-review.js:3224629/35390

---
+++ blueprint-hr-review.js
@@ -0,0 +1,14 @@
+module.exports = {
+  start: [{"blueprint":"hr-review","type":"message","value":"start"}],
+  dialog: {},
+  ephemeral: {},
+  message: {
+    start : {
+      blocks: [
+        
+      ]
+    }
+  },
+  thread: {},
+  update: {}
+}
