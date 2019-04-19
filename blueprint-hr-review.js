1b3ce5e02f96b3682d6716dd3c6bcc098ccdc1f8
Glitch (greet-bot) <none>
2019-04-19 23:38:20 +0000

    🤑👯 Checkpoint ./blueprint-hr-review.js:3224629/15557

--- blueprint-hr-review.js
+++ blueprint-hr-review.js
@@ -1,11 +1,210 @@
 module.exports = {
   start: [{"blueprint":"hr-review","type":"message","value":"start"}],
-  dialog: {},
+  dialog: {
+    feedback: {
+      "callback_id": "dialog:feedback",
+      "title": "Give feedback",
+      "submit_label": "Send feedback",
+      "elements": [
+        {
+          "label": "Select your colleague",
+          "type": "select",
+          "name": "reject:reason",
+          "options": [
+            {
+              "label": "Out of Policy",
+              "value": "reject:reason:out-of-policy"
+            },
+            {
+              "label": "Invalid request",
+              "value": "reject:reason:invalid-request"
+            },
+            {
+              "label": "Other",
+              "value": "reject:reason:other"
+            }
+          ]
+        },
+        {
+          "label": "Comment",
+          "name": "feedback",
+          "type": "textarea",
+          "optional": true
+        }
+      ],
+      "state": [{"blueprint":"approval","type":"update","value":"rejection","fill_values":["message.ts"]}]
+    }
+  },
   ephemeral: {},
   message: {
     start : {
       blocks: [
-        
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "*Hi David!* Here's how I can help you:"
+          }
+        },
+        {
+          "type": "divider"
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":dollar: *Pay*\nView latest pay info, history and summary"
+          },
+          "accessory": {
+            "type": "static_select",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Choose",
+              "emoji": true
+            },
+            "options": [
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Latest Pay",
+                  "emoji": true
+                },
+                "value": "pay"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Summary",
+                  "emoji": true
+                },
+                "value": "value-1"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Pay History",
+                  "emoji": true
+                },
+                "value": "value-1"
+              }
+            ]
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":speaking_head_in_silhouette: *Peer reviews*\nGive and receive feedback from your colleagues"
+          },
+          "accessory": {
+            "type": "static_select",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Choose",
+              "emoji": true
+            },
+            "options": [
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Give feedback",
+                  "emoji": true
+                },
+                "value": [{"blueprint":"hr-review","type":"dialog","value":"feedback"}]
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Request feedback",
+                  "emoji": true
+                },
+                "value": "value-1"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Your reviews",
+                  "emoji": true
+                },
+                "value": "value-1"
+              }
+            ]
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":clock1: *Time off*\nManage and view your time off requests"
+          },
+          "accessory": {
+            "type": "static_select",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Choose",
+              "emoji": true
+            },
+            "options": [
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Request time off",
+                  "emoji": true
+                },
+                "value": "pay"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "See requests",
+                  "emoji": true
+                },
+                "value": "value-1"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Your reviews",
+                  "emoji": true
+                },
+                "value": "value-1"
+              }
+            ]
+          }
+        },
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": ":gear: *Settings*\nNotifications, Account settings and more"
+          },
+          "accessory": {
+            "type": "static_select",
+            "placeholder": {
+              "type": "plain_text",
+              "text": "Choose",
+              "emoji": true
+            },
+            "options": [
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Notifications",
+                  "emoji": true
+                },
+                "value": "value-0"
+              },
+              {
+                "text": {
+                  "type": "plain_text",
+                  "text": "Choice 2",
+                  "emoji": true
+                },
+                "value": "value-1"
+              }
+            ]
+          }
+        }
       ]
     }
   },
