5f692a4bc1b83c7d5b0f761c5b1a98843129a581
Glitch (greet-bot) <none>
2019-05-01 00:21:14 +0000

    🍈🍵 Checkpoint ./README.md:3224629/2820 ./package.json:3224629/159 ./store.js:3224629/430 ./helpers.js:3224629/3444

--- slash-commands.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = {
-  "dpi-test": [{"blueprint":"help","type":"ephemeral","value":"help"}]
-}
---
+++ store.js
@@ -0,0 +1,21 @@
+// this is not a real store
+let messages = {}
+let users = {}
+
+exports.addMessage = (message) => {
+
+}
+
+exports.getMessages = () => {
+  return messages
+}
+
+exports.addUser = (message) => {
+
+}
+
+exports.getUser = (id) => {
+  return users[id]
+}
+
+exports.users = {}
