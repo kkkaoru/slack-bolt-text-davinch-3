de738d1dc9f216ec381dd02bac4008977c045be5
Glitch (greet-bot) <none>
2019-05-01 00:10:11 +0000

    🎣🐯 Checkpoint ./index.js:3224629/299

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ const helpers = require('./helpers')
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
-})
+});
+
+app.event('app_home_opened', ({ event, say }) => { 
+  console.log(event)
+  say('Hi')
+});
+
+app.event('reaction_added', ({ event, say }) => { 
+  console.log(event)
+});
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 3000)
 
-  console.log('⚡️ Bolt app is running!');
-})()
+  console.log('⚡️ Bolt app is running!')
+})();
 
