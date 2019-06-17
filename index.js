cf4369d5cd099e98048c43a98d45141b7916598e
Glitch (greet-bot) <none>
2019-06-17 18:03:32 +0000

    🍜💝 Checkpoint ./index.js:284076/321

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const { App } = require('@slack/bolt')
-
+const { App } = require('@slack/bolt');
+const store = require('./store');
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-  logLevel: 'DEBUG'
+  token: process.env.SLACK_BOT_TOKEN
 });
 
 /**
@@ -17,28 +16,24 @@ We use this event to show the user an interactive welcome message once they open
 
 **/
 app.event('app_home_opened', ({ event, say }) => {  
-  let user = store.getUser(event.user)
+  // Look up the user from DB
+  let user = store.getUser(event.user);
   
   if(!user) {
     user = {
       user: event.user,
       channel: event.channel
-    }
-    store.addUser(user)
+    };
+    store.addUser(user);
     
     say(messages.welcome_app_home)
   } 
-})
+});
 
-
-app.error((error) => {
-	// Check the details of the error to handle cases where you should retry sending a message or stop the app
-	console.error(error)
-})
 
 // Start your app
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
-})()
+})();
 
