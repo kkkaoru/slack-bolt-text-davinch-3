a929b714a35a889cd973946c1768ffcf3a403b1d
Glitch (greet-bot) <none>
2021-05-06 03:13:13 +0000

    🐜❤️ Checkpoint ./store.js:4268212/1055

--- store.js
+++ store.js
@@ -7,28 +7,28 @@ let defaultChannel = undefined;
 
 exports.getMessages = () => {
   return messages;
-}
+};
 
 exports.addUser = (user) => {
   users[user.user] = user;
-}
+};
 
 exports.getUser = (id) => {
   return users[id];
-}
+};
 
 exports.setChannel = (channel) => {
   defaultChannel = channel;
-}
+};
 
 exports.getChannel = () => {
   return defaultChannel;
-}
+};
 
 exports.setMe = (id) => {
   me = id;
-}
+};
 
 exports.getMe = () => {
   return me;
-}
+};
