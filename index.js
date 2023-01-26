c7cb4d2f3490e0b1f175cec09508974c1c50ba2e
Glitch (greet-bot) <none>
2023-01-26 12:37:44 +0000

    💧🛤 Checkpoint ./index.js:53807862/891

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
-// //メッセージが投稿された時に呼ばれるメソッド
-// app.message(async ({ event, message, say }) => {
-//   await say(`<@${event.user}> ${message.text}`);
-// });
-app.event('app_mention', async (event)=>{})
+//メッセージが投稿された時に呼ばれるメソッド
+app.message(async ({ message, say }) => {
+  await say(message.text);
+});
+
+// app.event('app_mention', async ({ event, context, client, say })=>{
+//   await say(`HELLO`);
+// })
 //アプリが起動時に呼ばれるメソッド
 (async () => {
   await app.start(process.env.PORT || 3000);
