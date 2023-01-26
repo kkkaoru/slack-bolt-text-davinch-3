a066c785fb5dfed9b10de0dbc464cd17dd4ab360
Glitch (greet-bot) <none>
2023-01-26 12:26:40 +0000

    💴🙋 Checkpoint ./index.js:53807862/987

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const { App } = require('@slack/bolt');
+const { App } = require("@slack/bolt");
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN
+  token: process.env.SLACK_BOT_TOKEN,
 });
-//メッセージが投稿された時に呼ばれるメソッド
-app.message(async ({ message, say }) => {
-  await say(message.text);
-});
+// //メッセージが投稿された時に呼ばれるメソッド
+// app.message(async ({ event, message, say }) => {
+//   await say(`<@${event.user}> ${message.text}`);
+// });
+app.event('app_mention', async (event)=>{})
 //アプリが起動時に呼ばれるメソッド
 (async () => {
   await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
