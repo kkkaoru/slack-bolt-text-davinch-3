0990a42bf1801656eca4af44573f3f723e175076
Glitch (greet-bot) <none>
2023-01-29 02:29:23 +0000

    🏦🌎 Checkpoint ./index.js:53807862/989 ./package.json:53807862/1799 ./fetch.js:53807862/1725

--- index.js
+++ index.js
@@ -5,41 +5,11 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
 });
-//メッセージが投稿された時に呼ばれるメソッド
-app.message(async ({ message, say }) => {
-  console.log('message log start');
-  console.log(message);
-  await say(message.text);
-  console.log('message log end');
-});
 
 app.event('app_mention', async ({ event, context, client, say }) => {
-  console.log('mention log start');
-  console.log(event);
-  console.log('--');
-  console.log(context);
-  console.log(trimMentions(event.text));
-  console.log('mention log end');
+  const message = trimMentions(event.text);
   try {
-    await say({"blocks": [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Thanks for the mention <@${event.user}>! Here's a button`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Button",
-            "emoji": true
-          },
-          "value": "click_me_123",
-          "action_id": "first_button"
-        }
-      }
-    ]});
+    await say(`<@${event.user}> ${message}`);
   }
   catch (error) {
     console.error(error);
